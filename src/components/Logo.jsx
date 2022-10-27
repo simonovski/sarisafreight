3e32827f984227dd1ca1d95403cc58985cd9d812
Aleksandar Simonovski <simonovski@gmail.com>
2022-10-28 00:00:23 +0200

    Initial commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+export function Logomark(props) {
+  return (
+    <img src="https://www.sarisafreight.com/wp-content/themes/sarisa/assets/images/logo.svg" />
+  )
+}
+
+export function Logo(props) {
+  return (
+    <img src="https://www.sarisafreight.com/wp-content/themes/sarisa/assets/images/logo.svg" />
+  )
+}
---
+++ src/components/SecondaryFeatures.jsx
@@ -0,0 +1,378 @@
+import { useId } from 'react'
+
+import { Container } from '@/components/Container'
+
+const features = [
+  {
+    name: 'Trucking operations',
+    description:
+      'We have our own fleet of trucks ready to deploy to assist our carriers operations.',
+    icon: DeviceArrowIcon,
+  },
+  {
+    name: 'Digital Logistics',
+    description:
+      'Real-time data of greight movement, driver performance metrics and many other tools to drive eficiencies.',
+    icon: DeviceCardsIcon,
+  },
+  {
+    name: 'Carrier network',
+    description:
+      'Our shared-values with the transportation ecosystem empowers us to provide the highest quaility freight operatos in the industry.',
+    icon: DeviceClockIcon,
+  },
+  {
+    name: 'Truck sales & Leasing',
+    description:
+      'We offer wide randge of truck salse and leasing solutions to help truckers grow and manage their business efficiently.',
+    icon: DeviceListIcon,
+  },
+  {
+    name: 'Truck Repair & Maintanance',
+    description:
+      'When the unpredictable happens, we have the resources to get truck back on the road.',
+    icon: DeviceLockIcon,
+  },
+  {
+    name: 'Finance & Insurance',
+    description:
+      "From factoring to liability protection, we've got our fleets coverd.",
+    icon: DeviceChartIcon,
+  },
+]
+
+function DeviceArrowIcon(props) {
+  return (
+    <svg viewBox="0 0 32 32" aria-hidden="true" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M9 0a4 4 0 00-4 4v24a4 4 0 004 4h14a4 4 0 004-4V4a4 4 0 00-4-4H9zm0 2a2 2 0 00-2 2v24a2 2 0 002 2h14a2 2 0 002-2V4a2 2 0 00-2-2h-1.382a1 1 0 00-.894.553l-.448.894a1 1 0 01-.894.553h-6.764a1 1 0 01-.894-.553l-.448-.894A1 1 0 0010.382 2H9z"
+        fill="#737373"
+      />
+      <path
+        d="M12 25l8-8m0 0h-6m6 0v6"
+        stroke="#171717"
+        strokeWidth={2}
+        strokeLinecap="round"
+      />
+      <circle cx={16} cy={16} r={16} fill="#A3A3A3" fillOpacity={0.2} />
+    </svg>
+  )
+}
+
+function DeviceCardsIcon(props) {
+  let id = useId()
+
+  return (
+    <svg viewBox="0 0 32 32" aria-hidden="true" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M9 0a4 4 0 00-4 4v24a4 4 0 004 4h14a4 4 0 004-4V4a4 4 0 00-4-4H9zm0 2a2 2 0 00-2 2v24a2 2 0 002 2h14a2 2 0 002-2V4a2 2 0 00-2-2h-1.382a1 1 0 00-.894.553l-.448.894a1 1 0 01-.894.553h-6.764a1 1 0 01-.894-.553l-.448-.894A1 1 0 0010.382 2H9z"
+        fill="#737373"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M9 13a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H10a1 1 0 01-1-1v-2zm0 6a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H10a1 1 0 01-1-1v-2zm1 5a1 1 0 00-1 1v2a1 1 0 001 1h12a1 1 0 001-1v-2a1 1 0 00-1-1H10z"
+        fill={`url(#${id}-gradient)`}
+      />
+      <rect x={9} y={6} width={14} height={4} rx={1} fill="#171717" />
+      <circle cx={16} cy={16} r={16} fill="#A3A3A3" fillOpacity={0.2} />
+      <defs>
+        <linearGradient
+          id={`${id}-gradient`}
+          x1={16}
+          y1={12}
+          x2={16}
+          y2={28}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#737373" />
+          <stop offset={1} stopColor="#737373" stopOpacity={0} />
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
+
+function DeviceClockIcon(props) {
+  return (
+    <svg viewBox="0 0 32 32" aria-hidden="true" {...props}>
+      <circle cx={16} cy={16} r={16} fill="#A3A3A3" fillOpacity={0.2} />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M5 4a4 4 0 014-4h14a4 4 0 014 4v10h-2V4a2 2 0 00-2-2h-1.382a1 1 0 00-.894.553l-.448.894a1 1 0 01-.894.553h-6.764a1 1 0 01-.894-.553l-.448-.894A1 1 0 0010.382 2H9a2 2 0 00-2 2v24a2 2 0 002 2h5v2H9a4 4 0 01-4-4V4z"
+        fill="#737373"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M24 32a8 8 0 100-16 8 8 0 000 16zm1-8.414V19h-2v5.414l4 4L28.414 27 25 23.586z"
+        fill="#171717"
+      />
+    </svg>
+  )
+}
+
+function DeviceListIcon(props) {
+  return (
+    <svg viewBox="0 0 32 32" fill="none" aria-hidden="true" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M9 0a4 4 0 00-4 4v24a4 4 0 004 4h14a4 4 0 004-4V4a4 4 0 00-4-4H9zm0 2a2 2 0 00-2 2v24a2 2 0 002 2h14a2 2 0 002-2V4a2 2 0 00-2-2h-1.382a1 1 0 00-.894.553l-.448.894a1 1 0 01-.894.553h-6.764a1 1 0 01-.894-.553l-.448-.894A1 1 0 0010.382 2H9z"
+        fill="#737373"
+      />
+      <circle cx={11} cy={14} r={2} fill="#171717" />
+      <circle cx={11} cy={20} r={2} fill="#171717" />
+      <circle cx={11} cy={26} r={2} fill="#171717" />
+      <path
+        d="M16 14h6M16 20h6M16 26h6"
+        stroke="#737373"
+        strokeWidth={2}
+        strokeLinecap="square"
+      />
+      <circle cx={16} cy={16} r={16} fill="#A3A3A3" fillOpacity={0.2} />
+    </svg>
+  )
+}
+
+function DeviceLockIcon(props) {
+  return (
+    <svg viewBox="0 0 32 32" aria-hidden="true" {...props}>
+      <circle cx={16} cy={16} r={16} fill="#A3A3A3" fillOpacity={0.2} />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M5 4a4 4 0 014-4h14a4 4 0 014 4v10h-2V4a2 2 0 00-2-2h-1.382a1 1 0 00-.894.553l-.448.894a1 1 0 01-.894.553h-6.764a1 1 0 01-.894-.553l-.448-.894A1 1 0 0010.382 2H9a2 2 0 00-2 2v24a2 2 0 002 2h5v2H9a4 4 0 01-4-4V4z"
+        fill="#737373"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M18 19.5a3.5 3.5 0 117 0V22a2 2 0 012 2v6a2 2 0 01-2 2h-7a2 2 0 01-2-2v-6a2 2 0 012-2v-2.5zm2 2.5h3v-2.5a1.5 1.5 0 00-3 0V22z"
+        fill="#171717"
+      />
+    </svg>
+  )
+}
+
+function DeviceChartIcon(props) {
+  return (
+    <svg viewBox="0 0 32 32" fill="none" aria-hidden="true" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M9 0a4 4 0 00-4 4v24a4 4 0 004 4h14a4 4 0 004-4V4a4 4 0 00-4-4H9zm0 2a2 2 0 00-2 2v24a2 2 0 002 2h14a2 2 0 002-2V4a2 2 0 00-2-2h-1.382a1 1 0 00-.894.553l-.448.894a1 1 0 01-.894.553h-6.764a1 1 0 01-.894-.553l-.448-.894A1 1 0 0010.382 2H9z"
+        fill="#737373"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M23 13.838V26a2 2 0 01-2 2H11a2 2 0 01-2-2V15.65l2.57 3.212a1 1 0 001.38.175L15.4 17.2a1 1 0 011.494.353l1.841 3.681c.399.797 1.562.714 1.843-.13L23 13.837z"
+        fill="#171717"
+      />
+      <path
+        d="M10 12h12"
+        stroke="#737373"
+        strokeWidth={2}
+        strokeLinecap="square"
+      />
+      <circle cx={16} cy={16} r={16} fill="#A3A3A3" fillOpacity={0.2} />
+    </svg>
+  )
+}
+
+export function SecondaryFeatures() {
+  return (
+    <section
+      id="secondary-features"
+      aria-label="Features for building a portfolio"
+      className="py-20 sm:py-32"
+    >
+      <Container>
+      <div class="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-cols-2 lg:items-start lg:gap-24 lg:px-8">
+          <div class="relative sm:py-16 lg:py-0">
+            <div>
+              <div>
+                <img
+                  class="h-11 w-auto bg-rose-100 py-2 px-4 rounded"
+                  src="https://www.sarisafreight.com/wp-content/themes/sarisa/assets/images/logo.svg"
+                  alt="Your Company"
+                />
+              </div>
+              <div class="mt-20">
+                <div>
+                  <a href="#" class="inline-flex space-x-4">
+                    <span class="rounded bg-rose-50 px-2.5 py-1 text-sm font-semibold text-rose-500">
+                    WELCOME TO SARISA
+                    </span>
+                    {/* <span class="inline-flex items-center space-x-1 text-sm font-medium text-rose-500">
+                      <span>Just shipped version 0.1.0</span>
+                      <svg
+                        class="h-5 w-5"
+                        x-description="Heroicon name: mini/chevron-right"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
+                    </span> */}
+                  </a>
+                </div>
+                <div class="mt-6 sm:max-w-xl">
+                  <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+                    What We Do
+                  </h1>
+                  <h3 class="text-xl font-bold tracking-tight text-gray-600 sm:text-2xl">
+                  We empower and strengthen the connection between shippers and carriers.
+                  </h3>
+                  <p class="mt-6 text-xl text-gray-500">
+                  We operate a carrier centric portfolio of businesses, founded under a common equity ownership, operating as integrated organizations. This is how we can scale more rapidly and ultimately provide the most efficient freight network possible.
+                  </p>
+                </div>
+                <form action="#" class="mt-12 sm:flex sm:w-full sm:max-w-lg">
+                  <div class="mt-4 sm:mt-0 sm:ml-3">
+                    <button
+                      type="submit"
+                      class="block w-full rounded-md border border-transparent bg-rose-500 px-5 py-3 text-base font-medium text-white shadow hover:bg-rose-600 focus:outline-none focus:ring-2 focus:ring-rose-500 focus:ring-offset-2 sm:px-10"
+                    >
+                      Contact Us
+                    </button>
+                  </div>
+                </form>
+                {/* <div class="mt-6">
+                  <div class="inline-flex items-center divide-x divide-gray-300">
+                    <div class="flex flex-shrink-0 pr-5">
+                      <svg
+                        class="h-5 w-5 text-yellow-400"
+                        x-description="Heroicon name: mini/star"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
+                      <svg
+                        class="h-5 w-5 text-yellow-400"
+                        x-description="Heroicon name: mini/star"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
+                      <svg
+                        class="h-5 w-5 text-yellow-400"
+                        x-description="Heroicon name: mini/star"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
+                      <svg
+                        class="h-5 w-5 text-yellow-400"
+                        x-description="Heroicon name: mini/star"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
+                      <svg
+                        class="h-5 w-5 text-yellow-400"
+                        x-description="Heroicon name: mini/star"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
+                    </div>
+                    <div class="min-w-0 flex-1 py-1 pl-5 text-sm text-gray-500 sm:py-3">
+                      <span class="font-medium text-gray-900">
+                        Rated 5 stars
+                      </span>{' '}
+                      by over{' '}
+                      <span class="font-medium text-rose-500">
+                        500 beta users
+                      </span>
+                    </div>
+                  </div>
+                </div> */}
+              </div>
+            </div>
+          </div>
+
+          <div class="relative sm:py-16 lg:py-0">
+            <div>
+
+              <div class="mt-20">
+
+                <div class="mt-6">
+                  <div class="inline-flex items-center divide-x divide-gray-300">
+                  <img src="https://www.sarisafreight.com/wp-content/uploads/2020/07/home-service-matrix-graphic@2x.png" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+        <ul
+          role="list"
+          className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-6 text-sm sm:mt-20 sm:grid-cols-2 md:gap-y-10 lg:max-w-none lg:grid-cols-3"
+        >
+          {features.map((feature) => (
+            <li
+              key={feature.name}
+              className="rounded-2xl border border-gray-200 p-8"
+            >
+              <feature.icon className="h-8 w-8" />
+              <h3 className="mt-6 font-semibold text-gray-900">
+                {feature.name}
+              </h3>
+              <p className="mt-2 text-gray-700">{feature.description}</p>
+            </li>
+          ))}
+        </ul>
+      </Container>
+    </section>
+  )
+}
